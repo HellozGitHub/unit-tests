45f0a40af737e6c5147f4b118fb5d1f226bedead
HellozGitHub <forever3580ya@gmail.com>
2016-03-20 22:21:53 +0800

    Update swap.c
    大幅度改動 （add an extra node front the head of the list）

--- swap.c
+++ swap.c
@@ -13,76 +13,58 @@ List *swap(List *head, List *node_1, List *node_2)
         (node_1 == node_2)) 
         return head;
 
-    int exist_2 = 0; 
+    int exit_2 = 0;
+    
+    List *extra,*pre_node_1 = NULL,*pre_node_2 = NULL,*tmp_node; //add a extra node 
+    extra = (List *) malloc(sizeof(entry));
+    extra->next = head;
+    tmp = extra;
 
-    List *_head = head;
-    List *pre_node_1 = NULL,*pre_node_2 = NULL,*tmp_node;
-
-    while (head && head->next && exist_2 != 2) { //bad performance (add exist_2 != 2 to stop)
-        if (head->next == node_1) { //if node1 == head, it cant work
-            pre_node_1 = head;
+    while (tmp && tmp->next && exist_2 != 2 ){ //if node_1 node_2 == head, it can also work
+        if (tmp->next == node_1) { 
+            pre_node_1 = tmp;
             exist_2 = exist_2+ 1;
         }
-
-        if (head->next == node_2) { //if node2 == head, it cant work
-            pre_node_2 = head;
+        if (tmp->next == node_2) {
+            pre_node_2 = tmp;
             exist_2 = exist_2 + 1;
         }
-        head = head->next;
+        tmp = tmp->next;
     }
-
-    head = _head;
-    if (head == node_1) { //handle the case : node1 == head (move to previous can get better performance)
-        pre_node_1 = NULL;
-        exist_2 = exist_2+ 1;
-    }
-
-    if (head == node_2) { //handle the case : node2 == head (move to previous can get better performance)
-        pre_node_2 = NULL;
-        exist_2 = exist_2+ 1;
-    }
-
+    
     if (exist_2 != 2) //cant find both or either node1 or node2
+        free(extra);
         return head;
-
-//swap process
-
-    if (pre_node_1 == NULL) { 
-        pre_node_2->next = node_1;
-        tmp_node = node_1->next;
-        node_1->next = node_2->next;
-        node_2->next = tmp_node;
-        return node_2;
-    }
-
-    if (pre_node_2 == NULL) {
-        pre_node_1->next = node_2;
-        tmp_node = node_2->next;
-        node_2->next = node_1->next;
-        node_1->next = tmp_node;
-        return node_1;
-    }
-
+ 
     if (node_2->next == node_1) {
         pre_node_2->next = node_1;
         tmp_node = node_1->next;
         node_1->next = node_2;
         node_2->next = tmp_node;
-        return head;
     }
 
-    if (node_1->next == node_2) {
+    else if (node_1->next == node_2) {
         pre_node_1->next = node_2;
         tmp_node = node_2->next;
         node_2->next = node_1;
         node_1->next = tmp_node;
+    }
+    else{
+        pre_node_1->next = node_2;
+        tmp_node = node_2->next;
+        node_2->next = node_1->next;
+        pre_node_2->next = node_1;
+        node_1->next = tmp_node;
+    }
+    if (extra == pre_node_1){ //indicate that the new head of the list is node_2
+        free(extra);
+        return node_2; 
+    }
+    else if (extra == pre_node_2){ //indicate that the new head of the list is node_1
+        free(extra);
+        return node_1;    
+    else{
+        free(extra);
         return head;
     }
-
-    pre_node_1->next = node_2;
-    tmp_node = node_2->next;
-    node_2->next = node_1->next;
-    pre_node_2->next = node_1;
-    node_1->next = tmp_node;
-    return head;
 }
