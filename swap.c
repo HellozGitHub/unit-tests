2a429a7bcf0037218b925315a8d6a1dda3faf813
HellozGitHub <forever3580ya@gmail.com>
2016-03-20 21:14:44 +0800

    Update swap.c
    modify 1

--- swap.c
+++ swap.c
@@ -4,50 +4,50 @@
 typedef struct List_node {
     int value;
     struct List_node *next;
-} List_node;
-
-typedef struct List_node List;
+}  List;
 
 List *swap(List *head, List *node_1, List *node_2)
 {
-    if (!head &&
-        (node_1 == NULL) && (node_2 == NULL) &&
-        (node_1 == node_2))
+    if (!head ||
+        (node_1 == NULL) || (node_2 == NULL) ||
+        (node_1 == node_2)) 
         return head;
 
-    int num_pre_node_1_and_node_2 = 0;
+    int exist_2 = 0; 
 
     List *_head = head;
-    List *pre_node_1,*pre_node_2,*tmp_node;
+    List *pre_node_1 = NULL,*pre_node_2 = NULL,*tmp_node;
 
-    while (head && head->next) {
-        if (head->next == node_1) {
+    while (head && head->next && exist_2 != 2) { //bad performance (add exist_2 != 2 to stop)
+        if (head->next == node_1) { //if node1 == head, it cant work
             pre_node_1 = head;
-            num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
+            exist_2 = exist_2+ 1;
         }
 
-        if (head->next == node_2) {
+        if (head->next == node_2) { //if node2 == head, it cant work
             pre_node_2 = head;
-            num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
+            exist_2 = exist_2 + 1;
         }
         head = head->next;
     }
 
     head = _head;
-    if (head == node_1) {
+    if (head == node_1) { //handle the case : node1 == head (move to previous can get better performance)
         pre_node_1 = NULL;
-        num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
+        exist_2 = exist_2+ 1;
     }
 
-    if (head == node_2) {
+    if (head == node_2) { //handle the case : node2 == head (move to previous can get better performance)
         pre_node_2 = NULL;
-        num_pre_node_1_and_node_2 = num_pre_node_1_and_node_2 + 1;
+        exist_2 = exist_2+ 1;
     }
 
-    if (num_pre_node_1_and_node_2 != 2)
+    if (exist_2 != 2) //cant find both or either node1 or node2
         return head;
 
-    if (pre_node_1 == NULL) {
+//swap process
+
+    if (pre_node_1 == NULL) { 
         pre_node_2->next = node_1;
         tmp_node = node_1->next;
         node_1->next = node_2->next;
